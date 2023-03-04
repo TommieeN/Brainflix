abab2d156230b8f60651206a3eed6c99d89d1ca9
TommyNguyen <timsnguyen@hotmail.com>
2023-03-03 22:55:24 -0800

    finish tablet. Video list images fixed.

--- src/components/MainVideoInfo/MainVideoInfo.jsx
+++ src/components/MainVideoInfo/MainVideoInfo.jsx
@@ -6,10 +6,12 @@ const MainVideoInfo = function ({activeVideo}) {
         <div className="main-info">
         <h1 className="main-info__header">{activeVideo.title}</h1>
         <div className="main-info__container">
-          <div className="main-info__date-title">
+          <div className="main-info__channel-date">
             <p className="main-info__item main-info__item--bold">By {activeVideo.channel}</p>
+            <p className="main-info__date">{timeStampConverter(activeVideo.timestamp)}</p>
+          </div>
+          <div className="main-info__wrapper">
             <p className="main-info__views">{activeVideo.views}</p>
-            <p className="main-info__dates">{timeStampConverter(activeVideo.timestamp)}</p>
             <p className="main-info__likes">{activeVideo.likes}</p>
           </div>
         </div>
